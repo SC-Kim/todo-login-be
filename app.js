41763f931121917a6996f6feac51be613d615e76
scKim <lansco1229@gmail.com>
2024-10-18 10:32:28 +0900

    first commit

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
+app.use(cors())
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
+console.log("mongouri", MONGODB_URI_PROD)
 app.use(bodyParser.json())
 app.use('/api', indexRouter)
 
-const mongoURI = 'mongodb://localhost:27017/todo-demo'
+const mongoURI = MONGODB_URI_PROD
 
 mongoose.connect(mongoURI, {useNewUrlParser:true}).then(() => {
     console.log("mongoose connected!!")
@@ -15,8 +20,8 @@ mongoose.connect(mongoURI, {useNewUrlParser:true}).then(() => {
     console.log("DB connection fail", err)
 })
 
-app.listen(5001, ()=>{
-    console.log("server on 5001")
+app.listen(5002, ()=>{      //5002 Atlas, 5001 Local 
+    console.log("server on 5002")
 })
 
 
